5eedc9e2e5130f88a72e542889c953fe395ac9ff
Vitaly Simonov <guardian909@rambler.ru>
2020-03-17 18:17:08 +0300

    Edit Map#componentDidUpdate

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -10,41 +10,44 @@ class Map extends React.PureComponent {
   }
 
   initializeMap() {
-    const {offers} = this.props;
+    if (document.getElementById(`map`)) {
+      const {offers} = this.props;
 
-    const city = [52.38333, 4.9];
+      const city = [52.38333, 4.9];
 
-    const icon = leaflet.icon({
-      iconUrl: `img/pin.svg`,
-      iconSize: [30, 30]
-    });
+      const icon = leaflet.icon({
+        iconUrl: `img/pin.svg`,
+        iconSize: [30, 30]
+      });
 
-    const iconActive = leaflet.icon({
-      iconUrl: `img/pin-active.svg`,
-      iconSize: [30, 30]
-    });
+      const iconActive = leaflet.icon({
+        iconUrl: `img/pin-active.svg`,
+        iconSize: [30, 30]
+      });
 
-    const zoom = 12;
+      const zoom = 12;
 
-    const map = leaflet.map(`map`, {
-      center: city,
-      zoom,
-      zoomControl: false,
-      marker: true
-    });
-    map.setView(city, zoom);
+      const map = leaflet.map(`map`, {
+        center: city,
+        zoom,
+        zoomControl: false,
+        marker: true
+      });
+      map.setView(city, zoom);
 
-    leaflet
-    .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    })
-    .addTo(map);
+      leaflet
+      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
+      .addTo(map);
 
-    offers.forEach((offer) => {
-      let iconPin = offer.id === this.props.currentOfferId ? iconActive : icon;
+      offers.forEach((offer) => {
+        let iconPin = offer.id === this.props.currentOfferId ? iconActive : icon;
 
-      leaflet.marker(offer.cord, {icon: iconPin}).addTo(map);
-    });
+        leaflet.marker(offer.cord, {icon: iconPin}).addTo(map);
+      });
+    }
+
   }
 
   componentDidMount() {
@@ -52,9 +55,23 @@ class Map extends React.PureComponent {
   }
 
   componentWillUnmount() {
-    const container = leaflet.DomUtil.get(`map`);
-    // eslint-disable-next-line camelcase
-    container._leaflet_id = null;
+    if (document.getElementById(`map`)) {
+      const container = leaflet.DomUtil.get(`map`);
+      // eslint-disable-next-line camelcase
+      container._leaflet_id = null;
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentOfferId !== this.props.currentOfferId) {
+      if (document.getElementById(`map`)) {
+        const container = leaflet.DomUtil.get(`map`);
+        // eslint-disable-next-line camelcase
+        container._leaflet_id = null;
+      }
+
+      this.initializeMap();
+    }
   }
 
   render() {
